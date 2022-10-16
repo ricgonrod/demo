1bf54c307334fa2a4e8dd978809d8a98c382ccd7
ricgonrod <ricgonrod@gmail.com>
2022-10-16 19:23:49 +0200

    prueba

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,10 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hola amigo");
-		System.out.println("Cómo estás");
-		System.out.println("Muy bien gracias");
-		System.out.println("Chao");
-		System.out.println("estoy en rama prueba");
+	
+
 	}
 
 }
