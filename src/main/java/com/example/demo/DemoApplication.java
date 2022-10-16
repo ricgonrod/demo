726bbf8e0414aaa8e6c7f36db10059cd6f6e710d
ricgonrod <ricgonrod@gmail.com>
2022-10-16 19:03:44 +0200

    Respondiendo saludo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hola amigo");
 		System.out.println("Cómo estás");
+		System.out.println("Muy bien gracias");
 	}
 
 }
